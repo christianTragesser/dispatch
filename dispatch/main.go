6de5b7911c9fbd52b93276b6eaf5dce771634568
Christian Tragesser <christian.tragesser@wwt.com>
2022-09-22 14:19:26 -0500

    upgrade kops to v1.24.3, kubernetes v1.24.6

--- dispatch/main.go
+++ dispatch/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	k8sVersion   string = "1.23.5"
-	kopsVersion  string = "1.23.1"
+	k8sVersion   string = "1.24.6"
+	kopsVersion  string = "1.24.3"
 	smallEC2     string = "t2.medium"
 	mediumEC2    string = "t2.xlarge"
 	largeEC2     string = "m4.2xlarge"
