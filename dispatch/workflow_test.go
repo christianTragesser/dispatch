55ea44b55012b0100dd2d22381e296e73daf0a96
Christian Tragesser <christian.tragesser@wwt.com>
2022-11-17 15:35:35 -0600

    remove KOPS related tests

--- dispatch/workflow_test.go
+++ dispatch/workflow_test.go
@@ -2,12 +2,7 @@ package dispatch
 
 import (
 	"os"
-	"testing"
 )
-
-type mockKopsEvent struct {
-	Size string
-}
 
 type mockTUIEvent struct {
 	action, FQDN, datestamp string
@@ -40,153 +35,8 @@ func (e mockTUIEvent) getClusterCreationDate(bucket string, cluster string) stri
 	return e.datestamp
 }
 
-func TestTUIWorkflow(t *testing.T) {
-	var event KopsEvent
-
-	tests := []struct {
-		expectedReturn KopsEvent
-		name           string
-		event          mockTUIEvent
-	}{
-		{
-			name: "create defaults",
-			event: mockTUIEvent{
-				action:        "create",
-				createDetails: []string{"dispatch.k8s.local", "small", "2"},
-			},
-			expectedReturn: KopsEvent{
-				Action:   "create",
-				FQDN:     "dispatch.k8s.local",
-				Size:     "small",
-				Count:    "2",
-				Version:  k8sVersion,
-				Verified: false,
-			},
-		},
-		{
-			name: "create large testy.cluster.io",
-			event: mockTUIEvent{
-				action:        "create",
-				createDetails: []string{"testy.cluster.io", "large", "20"},
-			},
-			expectedReturn: KopsEvent{
-				Action:   "create",
-				FQDN:     "testy.cluster.io",
-				Size:     "large",
-				Count:    "20",
-				Version:  k8sVersion,
-				Verified: false,
-			},
-		},
-		{
-			name: "delete defaults",
-			event: mockTUIEvent{
-				action:   "delete",
-				FQDN:     "dispatch.k8s.local",
-				clusters: []string{"dispatch.k8s.local"},
-			},
-			expectedReturn: KopsEvent{
-				Action:   "delete",
-				FQDN:     "dispatch.k8s.local",
-				Verified: false,
-			},
-		},
-		{
-			name: "delete testy.cluster.io",
-			event: mockTUIEvent{
-				action:   "delete",
-				FQDN:     "testy.cluster.io",
-				clusters: []string{"dispatch.k8s.local", "testy.cluster.io", "dont.delete.io"},
-			},
-			expectedReturn: KopsEvent{
-				Action:   "delete",
-				FQDN:     "testy.cluster.io",
-				Verified: false,
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			teAPI := test.event
-			testEvent := TUIWorkflow(teAPI, event)
-
-			if testEvent != test.expectedReturn {
-				t.Errorf("TUIWorkflow unit test failure '%s'\n got: '%v'\n want: '%v'", test.name, testEvent, test.expectedReturn)
-			}
-		})
-	}
-}
-
-func TestCLIWorkflow(t *testing.T) {
-	// provide dispatch subcommand of 'create' or 'delete'
-	// return kops event containing subcommand flag values
-	var event KopsEvent
-
-	tests := []struct {
-		expectedReturn KopsEvent
-		name           string
-		osargs         []string
-	}{
-		{
-			name:   "create defaults",
-			osargs: []string{"dispatch", "create"},
-			expectedReturn: KopsEvent{
-				Action:   "create",
-				FQDN:     "dispatch.k8s.local",
-				Size:     "small",
-				Count:    "2",
-				Version:  k8sVersion,
-				Verified: false,
-			},
-		},
-		{
-			name:   "create k8s v1.20.8 large testy.cluster.io yolo",
-			osargs: []string{"dispatch", "create", "-fqdn", "testy.cluster.io", "-size", "large", "-nodes", "20", "-version", "1.20.8", "-yolo", "true"},
-			expectedReturn: KopsEvent{
-				Action:   "create",
-				FQDN:     "testy.cluster.io",
-				Size:     "large",
-				Count:    "20",
-				Version:  "1.20.8",
-				Verified: true,
-			},
-		},
-		{
-			name:   "delete defaults",
-			osargs: []string{"dispatch", "delete", "-fqdn", "dispatch.k8s.local"},
-			expectedReturn: KopsEvent{
-				Action:   "delete",
-				FQDN:     "dispatch.k8s.local",
-				Verified: false,
-			},
-		},
-		{
-			name:   "delete testy.cluster.io yolo",
-			osargs: []string{"dispatch", "delete", "-fqdn", "testy.cluster.io", "-yolo", "true"},
-			expectedReturn: KopsEvent{
-				Action:   "delete",
-				FQDN:     "testy.cluster.io",
-				Verified: true,
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-			os.Args = test.osargs
-			testEvent := CLIWorkflow("test-Version", event)
-
-			if testEvent != test.expectedReturn {
-				t.Errorf("CLIWorkflow unit test failure '%s'\n got: '%v'\n want: '%v'", test.name, testEvent, test.expectedReturn)
-			}
-		})
-	}
-}
-
 func ExampleCLIWorkflow_version() {
-	var event KopsEvent
+	var event DispatchEvent
 
 	os.Args[1] = "Version"
 
@@ -196,7 +46,7 @@ func ExampleCLIWorkflow_version() {
 }
 
 func ExampleCLIWorkflow_help() {
-	var event KopsEvent
+	var event DispatchEvent
 
 	os.Args[1] = "-h"
 
@@ -208,7 +58,7 @@ func ExampleCLIWorkflow_help() {
 }
 
 func ExampleCLIWorkflow_deleteHelp() {
-	var event KopsEvent
+	var event DispatchEvent
 
 	os.Args = []string{"dispatch", "delete"}
 
@@ -218,7 +68,7 @@ func ExampleCLIWorkflow_deleteHelp() {
 }
 
 func ExampleCLIWorkflow_notValid() {
-	var event KopsEvent
+	var event DispatchEvent
 
 	os.Args[1] = "none"
 
@@ -231,7 +81,7 @@ func ExampleCLIWorkflow_notValid() {
 
 func ExampleTUIWorkflow_notValid() {
 	teAPI := mockTUIEvent{}
-	testEvent := KopsEvent{Action: "test"}
+	testEvent := DispatchEvent{Action: "test"}
 
 	TUIWorkflow(teAPI, testEvent)
 
