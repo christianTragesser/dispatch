b7e277955a8de6546f8580cf4a59cbc984629f57
Christian Tragesser <christian@evoen.net>
2024-06-07 07:34:56 -0500

    pulumi v3.117.0

--- dispatch/instance.go
+++ dispatch/instance.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	pulumiVersion    string = "3.115.0"
+	pulumiVersion    string = "3.117.0"
 	smallEC2         string = "t2.medium"
 	mediumEC2        string = "t2.xlarge"
 	largeEC2         string = "m4.2xlarge"
